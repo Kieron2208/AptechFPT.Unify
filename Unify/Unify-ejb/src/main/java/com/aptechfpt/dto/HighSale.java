ffca9e2739f2f3992d7edc945e707890e208b962
Trần Hữu Quang <kieron2208@gmail.com>
2015-12-20 11:19:26 +0700

    Prepare to merge

---
+++ Unify/Unify-ejb/src/main/java/com/aptechfpt/dto/HighSale.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aptechfpt.dto;
+
+/**
+ *
+ * @author ken
+ */
+public class HighSale {
+     public int quantity;
+    public String productName;
+    public HighSale() {
+        
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
